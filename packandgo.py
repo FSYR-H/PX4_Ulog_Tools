89a2ae20d687bd2a8204daf12d317c22d96b221e
FSYR-H <www.yesiyuan@live.com>
2024-02-22 17:44:09 +0800

    增加打包文件

---
+++ packandgo.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+
+# 指定你要打包的 Python 文件的路径
+python_file_path = 'test.py'
+
+# 构造 pyinstaller 命令
+command = f'pyinstaller --onefile --noconsole --name PX4_tools {python_file_path}'
+
+# 使用 subprocess 运行命令
+subprocess.run(command, shell=True)

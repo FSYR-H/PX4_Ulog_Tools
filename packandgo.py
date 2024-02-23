880d22220f5f1c434960492ba33a6b75a04b5cfe
FSYR-H <www.yesiyuan@live.com>
2024-02-23 17:59:44 +0800

    #增加功率计算

--- packandgo.py
+++ packandgo.py
@@ -5,7 +5,7 @@ import os
 python_file_path = 'test.py'
 
 # 构造 pyinstaller 命令
-command = f'pyinstaller --onefile --clean --icon=123123.ico --noconsole --name PX4_tools {python_file_path}'
+command = f'pyinstaller --onefile --icon=123123.ico --noconsole --name PX4_tools {python_file_path}'
 
 # 使用 subprocess 运行命令
 subprocess.run(command, shell=True)

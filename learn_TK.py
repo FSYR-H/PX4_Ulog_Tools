7a2b894fedb9a50b415136407a95573df8aa04ae
FSYR-H <www.yesiyuan@live.com>
2024-04-07 21:58:45 +0800

    #学习下拉框

--- learn_TK.py
+++ learn_TK.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk  #导入ttk模块，因为Combobox下拉菜单控件在ttk中
 
-def creat_com(com=None):
+def creat_combobox(com=None):
 
     str = "请点击下拉框选择："
     label = ttk.Label(root, text=str)
@@ -12,13 +12,21 @@ def creat_com(com=None):
 
     # 设置下拉菜单中的值
     combobox['value'] = ("北京","上海","广州","深圳")
-
+    combobox['state'] = "readonly"
     # 设置下拉菜单的默认值,默认值索引从0开始
     combobox.current(2)
 
     combobox.pack(side=TOP, anchor=NW)
     label.pack(side=TOP, anchor=NW)
+
     return combobox,label
+
+def xFunc(event,combobox):
+    print(str(combobox)[-1])
+    print(combobox.get())            # #获取选中的值方法1
+
+
+
 
 
 if __name__ == '__main__':
@@ -27,9 +35,11 @@ if __name__ == '__main__':
     root.title("combobox demo")
     root.geometry("1300x500")
 
-    com1, label1 = creat_com()
-    com2, label2 = creat_com()
-    com3, label3 = creat_com()
+    com1, label1 = creat_combobox()
+    
+    com2, label2 = creat_combobox()
+    com2.bind("<<ComboboxSelected>>", lambda event: xFunc(event, com2))
+    com3, label3 = creat_combobox()
 
 
     mainloop()
